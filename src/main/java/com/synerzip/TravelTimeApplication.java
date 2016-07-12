71e3b6dbdcc9671ccc08987cf6648a8603b73f7f
hussain_synerzip <hussain.pithawala@synerzip.com>
2016-07-12 12:34:21 +0530

    essential files

---
+++ src/main/java/com/synerzip/TravelTimeApplication.java
@@ -0,0 +1,12 @@
+package com.synerzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelTimeApplication.class, args);
+	}
+}
