1b33b21e3e3c8ee04c5ca9304f6ecab1f1815cbb
hussainp <hussain.pithawala@hotmail.com>
2016-07-14 17:45:18 +0530

    RestTemplate, supplier-properties and usage example

--- src/main/java/com/synerzip/TravelTimeApplication.java
+++ src/main/java/com/synerzip/TravelTimeApplication.java
@@ -4,17 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
 
 import com.synerzip.infra.security.ApplicationSecurity;
 
 @SpringBootApplication
 @Configuration
+@PropertySource("supplier.properties")
 public class TravelTimeApplication {	
 	@Bean
     public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter(){
     	return new ApplicationSecurity();
     }
+	
+	@Bean
+	public RestTemplate provideRestTemplate(){
+		return new RestTemplate();
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TravelTimeApplication.class, args);
--- src/main/java/com/synerzip/client/rest/FirstRestService.java
+++ src/main/java/com/synerzip/client/rest/FirstRestService.java
@@ -1,19 +1,43 @@
 package com.synerzip.client.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.synerzip.models.LowFareFlightSearchRS;
 
 @RequestMapping("/rest")
 @RestController
 public class FirstRestService {
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private Logger logger = LoggerFactory.getLogger(FirstRestService.class);
 	
 	@RequestMapping(value = "/hello", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<SimpleResponse> hello() {
 		return new ResponseEntity<SimpleResponse>(new SimpleResponse("Happy Coding with Spring...."), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/searchLowFare", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<LowFareFlightSearchRS> searchLowFareFlights() {
+		StringBuilder url = new StringBuilder("http://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?origin=LHR&destination=JFK&departure_date=2016-07-30&return_date=2016-08-07&number_of_results=3&apikey=");
+		url.append(env.getProperty("amadeus.api.key"));
+		
+		logger.info(url.toString());
+		
+		return new ResponseEntity<LowFareFlightSearchRS>(restTemplate.getForObject(url.toString(), LowFareFlightSearchRS.class), HttpStatus.OK);
 	}
 	
 	public static class SimpleResponse {
